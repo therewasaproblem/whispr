c0b2677012eee265147e6f84f065ca30b2e36b82
João Seike <joaogseike@gmail.com>
2019-09-02 22:32:12 -0300

    minor changes

--- ScriptsDaAplicação/crawlerSample.py
+++ ScriptsDaAplicação/crawlerSample.py
@@ -8,16 +8,20 @@ plain_text = source_code.text
 soup = BeautifulSoup(plain_text, 'xml')
 titles = soup.find_all('title')
 
-string = "Bolsonaro"
+string = "Amazonia"
 
 # Return every headline available
+
+
 def returnHeadlines():
     print("TODAS AS NOTICIAS: \
-    ")
+")
     for titulo in titles:
         print(titulo)
 
 # Return the headlines which have the given string parameter
+
+
 def returnParameterHeadlines(string):
     print("NOTICIAS COM A PALAVRA " + string + ":")
     for titulo2 in titles:
