64928caa56d681c9ac620ffe8762a73a079cc995
Ramon Dantas <dts.ramon@gmail.com>
2019-09-11 15:55:09 -0300

    👌
    Co-Authored-By: Lucas Pereira <lucas.pcbranco@hotmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: 'Hello! Welcome to WHISPR, a place where you will find recent AND trustful contents about your favourite and most searched news. Happy News! Literally..'
+        render html: 'Hello! Welcome to WHISPR, a place where you will find recent AND trustful contents about your favourite and most searched news. Good News, everyone! Literally..'
     end
 end
