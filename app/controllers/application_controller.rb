f7e0519505beab0859ac7ec5258c1100c431989c
ekisu <dts.ramon@gmail.com>
2019-09-19 20:20:25 -0300

    views: adicionar bootstrap
    Co-authored-by: funnyprogrammer <joon.kim@usp.br>
    Co-authored-by: kex1998 <kkelvin2@hotmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: 'Hello! Welcome to WHISPR, a place where you will find recent AND trustful contents about your favourite and most searched news. Good News, everyone! Literally..'
+        render html: 'teste aqui'
     end
 end
