d951b44bc259895cfaba47100ba5d0e4d8de0644
kex1998 <kkelvin2@usp.br>
2019-09-24 14:09:41 -0300

    Atualizacao na pagina inicial (falta acrescentar caminhos para cadastro e login)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    root 'welcome#hello'
+    root 'application#hello'
     devise_for :users
     get "/rss", to: "rss#index"
     get "/rss/search", to: "rss#search"
