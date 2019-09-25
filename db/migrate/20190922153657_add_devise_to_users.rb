b121da42b0103de0a829ec307917a7b842b087c4
Lucas Pereira <lucas.pcbranco@hotmail.com>
2019-09-24 23:28:58 -0300

    Cadastro, login e logout 100% funcionais.

--- db/migrate/20190922153657_add_devise_to_users.rb
+++ db/migrate/20190922153657_add_devise_to_users.rb
@@ -12,7 +12,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       t.datetime :reset_password_sent_at
 
       ## Rememberable
-      # t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -44,6 +44,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    #raise ActiveRecord::IrreversibleMigration
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_09_22_153657) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
