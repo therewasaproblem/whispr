e17da4e06d14fdb2d9aba353efd8c4913e69c752
ekisu <dts.ramon@gmail.com>
2019-09-26 18:08:15 -0300

    models/news: adicionar teste de unicidade

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not allow 2 news with the same url, title and summary" do
+    first = News.new title: "X", summary: "X", url: "X"
+    expect(first.save).to be true
+
+    second = News.new title: "X", summary: "X", url: "X"
+    expect(second.save).to be false
+  end
 end
