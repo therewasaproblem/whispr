e6e6d7e9e8118ad7fd194d29f84164eda097a03e
ekisu <dts.ramon@gmail.com>
2019-09-27 13:47:34 -0300

    controllers/rss: renomear para NewsController, utilizar o model News
    big commits big prizes

--- spec/helpers/news_helper_spec.rb
+++ spec/helpers/news_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the RssHelper. For example:
+# the NewsHelper. For example:
 #
-# describe RssHelper do
+# describe NewsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe RssHelper, type: :helper do
+RSpec.describe NewsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
