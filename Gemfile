2af2502502028db508697940cb2f106a82fd1a09
ekisu <dts.ramon@gmail.com>
2019-10-22 14:00:11 -0300

    cucumber: adicionar suporte ao FactoryBot e WebKit
    O driver WebKit do Capybara é mais fácil de instalar, e tem melhor
    performance que o Selenium (https://thoughtbot.com/blog/capybara-webkit)

--- Gemfile
+++ Gemfile
@@ -66,9 +66,10 @@ group :development do
 end
 
 group :test do
-  # Adds support for Capybara system testing and selenium driver
+  # Adds support for Capybara system testing and webkit driver
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
+  gem 'selenium-webdriver' # XXX Aparentemente o Cucumber/Capybara quebra sem isso?
+  gem 'capybara-webkit'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
   # Add Sidekiq testing support to RSpec
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,9 @@
 # files.
 
 require 'cucumber/rails'
+require 'factory_bot_rails'
+
+World(FactoryBot::Syntax::Methods)
 
 # frozen_string_literal: true
 
@@ -12,6 +15,7 @@ require 'cucumber/rails'
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
+Capybara.javascript_driver = :webkit
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how
