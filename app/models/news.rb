b60299ab7b0eb0f21fb1a11ef23f41027dae6044
guilherme <guigo.cerda@usp.br>
2019-10-22 22:23:08 -0300

    Arrumado testes e devolvida barra de pesquisa XD

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,6 @@
 class News < ApplicationRecord
     validates_uniqueness_of :url, scope: [:title, :summary]
-    belongs_to :sources
-    belongs_to :categories
+    belongs_to :sources, required: false
+    belongs_to :categories, required: false
     has_and_belongs_to_many :users
 end
