05cb4afc7117f7f9dc920db9fa9bc3e643ff2a47
Lucas Pereira <lucas.pcbranco@hotmail.com>
2019-11-03 21:25:50 -0300

    Consertando Cucumber!

--- features/step_definitions/home_page.rb
+++ features/step_definitions/home_page.rb
@@ -1,12 +1,15 @@
 Given /^I am on the home page and not signed in$/ do
-  visit "/"
+  visit '/'
 end
- 
+
 Then /^I should see "([^"]*)"$/ do |text|
   page.should have_content text
 end
 
-
 Given /^I am on the home page and signed in$/ do
-  visit "/"
+  User.create(:email => "test@cucumber.com", :password => "123456")
+  visit '/users/sign_in'
+  fill_in "user_email", :with => "test@cucumber.com"
+  fill_in "user_password", :with => "123456"
+  click_button "Log in"
 end
