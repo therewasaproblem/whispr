3fa2dba126bf60581fc585482e85d2161bbf0c6f
kex1998 <kkelvin2@usp.br>
2019-11-05 17:54:08 -0300

    Teste ajustado Collab Ramon em tudin

--- spec/workers/update_all_sources_worker_spec.rb
+++ spec/workers/update_all_sources_worker_spec.rb
@@ -5,14 +5,18 @@ Sidekiq::Testing.fake!
 RSpec.describe UpdateAllSourcesWorker, type: :worker do
     before(:each) {
         # Criar as sources de teste.
+	tec = Category.create(tag: "Tecnologia", description: "Fontes a respeito de Tecnologias")
+	
         @sources = [
             Source.create(name: "Ruby", language: "en",
                       homepage_url: "https://www.ruby-lang.org/",
-                      feed_url: file_fixture("ruby_lang.rss")),
+                      feed_url: file_fixture("ruby_lang.rss"),
+		      category: tec),
             Source.create(name: "UOL", language: "pt-br",
                       homepage_url: "https://www.uol.com.br/",
                       feed_url: file_fixture("uol.xml"),
-                      feed_options: { encoding: "Windows-1252" })
+                      feed_options: { encoding: "Windows-1252" },
+		      category: tec)
         ]
     }
 
