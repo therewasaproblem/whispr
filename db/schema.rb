ef6e23ae2d0eea58678a6de668a346d025772711
guilherme <guigo.cerda@usp.br>
2019-11-13 23:06:16 -0300

    Arrumando migração

--- db/schema.rb
+++ db/schema.rb
@@ -72,11 +72,12 @@ ActiveRecord::Schema.define(version: 2019_11_07_014640) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
