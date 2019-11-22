6a26f520fcecc20ebd1968dc15d667af7e9db3a1
funnyprogrammer <nicolas.hyuk@gmail.com>
2019-11-22 09:15:18 -0300

    Removendo a tabela user_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_114653) do
+ActiveRecord::Schema.define(version: 2019_11_22_121334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,13 +66,6 @@ ActiveRecord::Schema.define(version: 2019_11_22_114653) do
     t.bigint "user_id", null: false
     t.index ["source_id"], name: "index_sources_users_on_source_id"
     t.index ["user_id"], name: "index_sources_users_on_user_id"
-  end
-
-  create_table "user_categories", force: :cascade do |t|
-    t.bigint "users_id"
-    t.bigint "categories_id"
-    t.index ["categories_id"], name: "index_user_categories_on_categories_id"
-    t.index ["users_id"], name: "index_user_categories_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
